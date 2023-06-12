44ace9ce7b7568391c7be70815d29a614ed17751
Mihai <mihaismoleac2002@gmail.com>
2023-06-12 13:25:42 +0300

    last commit

---
+++ sisiLab2/main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from scipy.interpolate import make_interp_spline
+import numpy as np
+import datetime as DT
+from matplotlib.dates import date2num
+
+
+if __name__ == '__main__':
+    # task1()
+    # task2()
+    # task3()
+    # task4()
+    # task5()
+    # task6()
+    # task7()
+    # task8()
+    # task9()
+    # task10()
+    # task11()
+    # task12()
+    # task13()
+    # task14()
+    # task15()
+    # exercise1()
+    # exercise2()
+    # exercise3()
+    # exercise4()
+    # exercise5()
+    # exercise6()
+    # exercise7()
+    # exercise8()
+    # exercise9()
+    exercise10()
